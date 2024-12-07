5b7bd9027e355d09b63419a0cda9bace76d93506
Daniele Muffato <danixgithub1@gmail.com>
2024-12-07 21:47:05 +0100

    added startup script

---
+++ downloads/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+{
+    {
+        date "+waiting for connection: %H:%m:%S:%N"
+        nm-online &>/dev/null
+        date "+connection resolved (or failed): %H:%m:%S:%N"
+        /personal/repos/daniele821/dotfiles/scripts/git_repos/update_git_repos.sh
+        date "+git repos updated: %H:%m:%S:%N"
+    } >/tmp/GIT_UPDATES.out &
+
+    # add more startups here
+
+} &>/dev/null
