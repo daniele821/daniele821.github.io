ff255cee6554f7aceee5716f09cf7cfa94c0cabe
Daniele Muffato <danixgithub1@gmail.com>
2024-12-09 11:10:57 +0100

    updated startup script

--- downloads/startup.sh
+++ downloads/startup.sh
@@ -3,7 +3,7 @@
 {
     {
         date "+waiting for connection: %H:%m:%S:%N"
-        if nm-online -t 10; then
+        if nm-online; then
             date "+connection resolved: %H:%m:%S:%N"
             /personal/repos/daniele821/dotfiles/scripts/git_repos/update_git_repos.sh
             date "+git repos updated: %H:%m:%S:%N"
