c215569b1c4b0f36d9865e57c39eab0ad6cf7d55
Daniele Muffato <danixgithub1@gmail.com>
2025-01-20 20:45:37 +0100

    added option to reboot

--- downloads/installer.sh
+++ downloads/installer.sh
@@ -29,19 +29,25 @@ function run_git() {
     git clone git@daniele821.github.com:daniele821/dotfiles.git "$(mktemp -d /tmp/fix-ssh-keyXXXXXXXXXXXXXXXXX)"
     ./dotfiles/scripts/git_repos/restore_git_repos.sh
 }
+function run_reboot() {
+    ask_user "Are you REALLY REALLY sure you want to reboot?" && reboot
+}
 
 if [[ "$#" -gt 1 ]]; then
     for word in "$@"; do
         [[ "$word" == "init" ]] && export RUN_INIT=y
         [[ "$word" == "git" ]] && export RUN_GIT=y
+        [[ "$word" == "reboot" ]] && export RUN_REBOOT=y
     done
 fi
 
 if [[ "$#" -gt 1 ]]; then
     [[ "${RUN_INIT}" == "y" ]] && run_init
     [[ "${RUN_GIT}" == "y" ]] && run_git
+    [[ "${RUN_REBOOT}" == "y" ]] && run_reboot
 else
     [[ -z "${BRANCH}" ]] && read -p "Write branch name: " -r branch </dev/tty && BRANCH="$branch"
     ask_user "Do you really want to run init scripts?" && run_init
     ask_user "Do you really want to download git repos?" && run_git
+    ask_user "Do you really want to reboot?" && run_reboot
 fi
