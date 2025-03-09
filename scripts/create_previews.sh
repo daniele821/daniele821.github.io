8ab881cfe970cf403198e8756c607397f83ec33a
Daniele Muffato <danixgithub1@gmail.com>
2025-03-09 04:01:19 +0100

    autodelete old files

--- scripts/create_previews.sh
+++ scripts/create_previews.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 
 JSON_FILE="${SCRIPT_DIR}/scripts.json"
 FILE_NAMES=()
+TMP_DIR="$(mktemp -d)"
 
 mkdir -p "${SCRIPT_DIR}/previews"
 mkdir -p "${SCRIPT_DIR}/.previews_original"
@@ -12,12 +13,17 @@ mkdir -p "${SCRIPT_DIR}/.previews_original"
 for ORIGINAL in "${SCRIPT_DIR}/downloads/"*; do
     ORIGINAL_BASE="$(basename "${ORIGINAL}")"
     ORIGINAL_EXT="${ORIGINAL_BASE%.*}"
+    NEW_FILE="${SCRIPT_DIR}/previews/${ORIGINAL_EXT}.html"
     OLD_VER="${SCRIPT_DIR}/.previews_original/$ORIGINAL_BASE"
-    if ! diff -q "$ORIGINAL" "$OLD_VER" &>/dev/null; then
-        nvim "${ORIGINAL}" --headless -c "TOhtml ${SCRIPT_DIR}/previews/${ORIGINAL_EXT}.html | qa"
+    if ! [[ -f "$NEW_FILE" ]] || ! diff -q "$ORIGINAL" "$OLD_VER" &>/dev/null; then
+        nvim "${ORIGINAL}" --headless -c "TOhtml $NEW_FILE | qa"
         cp "$ORIGINAL" "$OLD_VER"
     fi
+    cp "$NEW_FILE" "${TMP_DIR}/"
     FILE_NAMES+=("$ORIGINAL_EXT")
 done
 
 printf '%s\n' "${FILE_NAMES[@]}" | jq -R . | jq -s . >"$JSON_FILE"
+
+rm -rf "${SCRIPT_DIR}/previews/"
+mv "$TMP_DIR/" "${SCRIPT_DIR}/previews"
