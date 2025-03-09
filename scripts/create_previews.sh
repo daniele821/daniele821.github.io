c18e15a7b01031e99860e593ffee076147e340cf
Daniele Muffato <danixgithub1@gmail.com>
2025-03-09 03:39:19 +0100

    automated index.html script previews

--- scripts/create_previews.sh
+++ scripts/create_previews.sh
@@ -3,6 +3,9 @@
 SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 
+JSON_FILE="${SCRIPT_DIR}/scripts.json"
+FILE_NAMES=()
+
 {
     rm "${SCRIPT_DIR}/previews/"* "${SCRIPT_DIR}/previews/".*
     mkdir -p "${SCRIPT_DIR}/previews"
@@ -12,4 +15,7 @@ for ORIGINAL in "${SCRIPT_DIR}/downloads/"*; do
     ORIGINAL_BASE="$(basename "${ORIGINAL}")"
     ORIGINAL_EXT="${ORIGINAL_BASE%.*}"
     nvim "${ORIGINAL}" --headless -c "TOhtml ${SCRIPT_DIR}/previews/${ORIGINAL_EXT}.html | qa"
+    FILE_NAMES+=("$ORIGINAL_EXT")
 done
+
+printf '%s\n' "${FILE_NAMES[@]}" | jq -R . | jq -s . >"$JSON_FILE"
