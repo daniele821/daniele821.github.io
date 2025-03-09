cd906720ec1f7d920557c474a9bd6cb5735fd092
Daniele Muffato <danixgithub1@gmail.com>
2025-03-09 03:47:53 +0100

    added functionality to check if file changed, and update their previews only in that case

--- scripts/create_previews.sh
+++ scripts/create_previews.sh
@@ -9,12 +9,17 @@ FILE_NAMES=()
 {
     rm "${SCRIPT_DIR}/previews/"* "${SCRIPT_DIR}/previews/".*
     mkdir -p "${SCRIPT_DIR}/previews"
+    mkdir -p "${SCRIPT_DIR}/.previews_original"
 } &>/dev/null
 
 for ORIGINAL in "${SCRIPT_DIR}/downloads/"*; do
     ORIGINAL_BASE="$(basename "${ORIGINAL}")"
     ORIGINAL_EXT="${ORIGINAL_BASE%.*}"
-    nvim "${ORIGINAL}" --headless -c "TOhtml ${SCRIPT_DIR}/previews/${ORIGINAL_EXT}.html | qa"
+    OLD_VER="${SCRIPT_DIR}/.previews_original/$ORIGINAL_BASE"
+    if ! diff -q "$ORIGINAL" "$OLD_VER" &>/dev/null; then
+        nvim "${ORIGINAL}" --headless -c "TOhtml ${SCRIPT_DIR}/previews/${ORIGINAL_EXT}.html | qa"
+        cp "$ORIGINAL" "$OLD_VER"
+    fi
     FILE_NAMES+=("$ORIGINAL_EXT")
 done
 
