9389280c118a56d9a84c761e146a5a6ab0bfaeca
Daniele Muffato <danixgithub1@gmail.com>
2025-03-25 19:36:02 +0100

    script almost works

--- update.sh
+++ update.sh
@@ -5,12 +5,23 @@ SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
 
 JSON_FILE="${SCRIPT_DIR}/scripts.json"
 
+INDEX=0
+
+echo "[" >"$JSON_FILE"
 find "${SCRIPT_DIR}" -mindepth 2 -maxdepth 2 -type f -iname 'index.html' |
     while read -r path; do
+        [[ "$INDEX" -ge 1 ]] && echo "," >>"$JSON_FILE"
         dir="$(dirname "$path")"
         update_path="$dir/update.sh"
         favicon_path="$dir/images/favicon.png"
         name="$(basename "$dir")"
         name="${name^}"
-        echo $path, $dir, $update_path, $favicon_path, $name
+        echo "{\"name\":\"$name\", \"img\":\"$favicon_path\", \"url\":\"$path\"}" | jq -c >>"$JSON_FILE"
+
+        if [[ -x "$update_path" ]]; then
+            echo "running script: $name"
+            "$update_path"
+        fi
+        ((INDEX++))
     done
+echo "]" >>"$JSON_FILE"
