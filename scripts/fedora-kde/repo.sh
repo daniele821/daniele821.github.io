751327592039908184ca4a2053f839bce6c7b528
Daniele Muffato <danixgithub1@gmail.com>
2025-07-22 14:29:12 +0200

    modify script to use ./autosaver git instead of 03 script

--- scripts/fedora-kde/repo.sh
+++ scripts/fedora-kde/repo.sh
@@ -1,5 +1,5 @@
 TMP_DIR="$(mktemp -d)" &&
     git clone --branch fedora-kde --depth 1 "https://github.com/daniele821/dotfiles" "$TMP_DIR" &&
-    "$TMP_DIR/run/03_gitrepos.sh" &&
+    "$TMP_DIR/autosaver" git &&
     rm -rf "$TMP_DIR"
 
