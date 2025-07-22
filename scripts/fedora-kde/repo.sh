b731e1dd3e08ea83e930780a4e2722a18e03d7e4
Daniele Muffato <danixgithub1@gmail.com>
2025-07-22 14:59:48 +0200

    fixed script bug

--- scripts/fedora-kde/repo.sh
+++ scripts/fedora-kde/repo.sh
@@ -1,7 +1,7 @@
 TMP_DIR="$(mktemp -d)" &&
     git clone --branch fedora-kde --depth 1 "https://github.com/daniele821/dotfiles" "$TMP_DIR" &&
     BRANCH="" "$TMP_DIR/autosaver" git &&
-    SET_BRANCH="" /personal/repos/daniele821/dotfiles &&
+    SET_BRANCH="" /personal/repos/daniele821/dotfiles/autosaver &&
     rm -rf ~/.config/nvim && ln -s /personal/repos/daniele821/nvim-config ~/.config/nvim &&
     rm -rf "$TMP_DIR"
 
