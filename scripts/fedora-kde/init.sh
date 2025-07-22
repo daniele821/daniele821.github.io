a456ec22d3c6f9763e8c276144c7ab42c8a3c2fd
Daniele Muffato <danixgithub1@gmail.com>
2025-07-22 14:33:39 +0200

    fixed scripts bug

--- scripts/fedora-kde/init.sh
+++ scripts/fedora-kde/init.sh
@@ -4,6 +4,6 @@ sudo -v && echo -e "\e[1;33mWARNING: make sure to connect usb drive with passwor
     wget -O "$ZIP_FILE" https://github.com/daniele821/dotfiles/archive/refs/heads/fedora-kde.zip &&
     unzip -d "$TMP_DIR" "$ZIP_FILE" && BRANCH="" "${TMP_DIR}/dotfiles-fedora-kde/autosaver" restoreall &&
     bash -lic "BRANCH= '${TMP_DIR}/dotfiles-fedora-kde/autosaver' run" &&
-    "${TMP_DIR}/dotfiles-fedora-kde/autosaver" git &&
+    BRANCH="" "${TMP_DIR}/dotfiles-fedora-kde/autosaver" git &&
     for i in {30..1}; do echo "rebooting in $i seconds... ctrl+c to skip" sleep 1; done &&
     reboot || echo -e "\e[1;31mfailed to download and run init scripts\e[m"
--- scripts/fedora-kde/repo.sh
+++ scripts/fedora-kde/repo.sh
@@ -1,5 +1,5 @@
 TMP_DIR="$(mktemp -d)" &&
     git clone --branch fedora-kde --depth 1 "https://github.com/daniele821/dotfiles" "$TMP_DIR" &&
-    "$TMP_DIR/autosaver" git &&
+    BRANCH="" "$TMP_DIR/autosaver" git &&
     rm -rf "$TMP_DIR"
 
