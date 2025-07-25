7b26a059e4a0251ddd159e7f0ba3129405e7b1e6
Daniele Muffato <danixgithub1@gmail.com>
2025-07-25 20:43:20 +0200

    added performance profile for init script

--- scripts/fedora-kde/init.sh
+++ scripts/fedora-kde/init.sh
@@ -1,5 +1,6 @@
 sudo -v && echo -e "\e[1;33mWARNING: make sure to connect usb drive with passwords...\e[0m" &&
     sleep 10 && { systemd-inhibit --what=idle:sleep sleep 3600 & } &&
+    tuned-adm profile throughput-performance &&
     TMP_DIR="$(mktemp -d)" && ZIP_FILE="${TMP_DIR}/dotfiles.zip" &&
     wget -O "$ZIP_FILE" https://github.com/daniele821/dotfiles/archive/refs/heads/fedora-kde.zip &&
     unzip -d "$TMP_DIR" "$ZIP_FILE" && BRANCH="" "${TMP_DIR}/dotfiles-fedora-kde/autosaver" restoreall &&
@@ -8,4 +9,5 @@ sudo -v && echo -e "\e[1;33mWARNING: make sure to connect usb drive with passwor
     SET_BRANCH="" /personal/repos/daniele821/dotfiles/autosaver &>/dev/null &&
     rm -rf ~/.config/nvim && ln -s /personal/repos/daniele821/nvim-config ~/.config/nvim &&
     for i in {30..1}; do echo "rebooting in $i seconds... ctrl+c to skip"; sleep 1; done &&
+    tuned-adm profile powersave &&
     reboot || echo -e "\e[1;31mfailed to download and run init scripts\e[m"
