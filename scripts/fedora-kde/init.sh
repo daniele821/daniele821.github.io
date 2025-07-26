af1323ab626f495c151fa8a8e9f95df51a655806
Daniele Muffato <danixgithub1@gmail.com>
2025-07-26 18:57:06 +0200

    added step to init nvim

--- scripts/fedora-kde/init.sh
+++ scripts/fedora-kde/init.sh
@@ -6,8 +6,8 @@ sudo -v && echo -e "\e[1;33mWARNING: make sure to connect usb drive with passwor
     unzip -d "$TMP_DIR" "$ZIP_FILE" && BRANCH="" "${TMP_DIR}/dotfiles-fedora-kde/autosaver" restoreall &&
     bash -lic "BRANCH= '${TMP_DIR}/dotfiles-fedora-kde/autosaver' run" &&
     BRANCH="" "${TMP_DIR}/dotfiles-fedora-kde/autosaver" git &&
-    SET_BRANCH="" /personal/repos/daniele821/dotfiles/autosaver &>/dev/null &&
-    rm -rf ~/.config/nvim && ln -s /personal/repos/daniele821/nvim-config ~/.config/nvim &&
+    SET_BRANCH="" /personal/repos/daniele821/dotfiles/autosaver &>/dev/null && rm -rf ~/.config/nvim &&
+    ln -s /personal/repos/daniele821/nvim-config ~/.config/nvim && nvim --headless +qa &&
     for i in {30..1}; do echo "rebooting in $i seconds... ctrl+c to skip"; sleep 1; done &&
     tuned-adm profile powersave &&
     reboot || echo -e "\e[1;31mfailed to download and run init scripts\e[m"
