7eea536e5d4be20abbccb6fddbcf94a339da0107
Daniele Muffato <danixgithub1@gmail.com>
2025-08-01 22:29:22 +0200

    fixed script

--- scripts/fedora-kde/repo.sh
+++ scripts/fedora-kde/repo.sh
@@ -1,4 +1,4 @@
 TMP_DIR="$(mktemp -d)" &&
     git clone --branch fedora-kde --depth 1 "https://github.com/daniele821/dotfiles" "$TMP_DIR" &&
-    BRANCH="" "$TMP_DIR/autosaver" git &&
+    NO_CHECKS="" "$TMP_DIR/autosaver" git &&
     rm -rf "$TMP_DIR" || echo -e "\e[1;31mfailed to download git repos\e[m"
