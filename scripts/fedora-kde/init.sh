863babab4b4f9a04938add8e925a6760e77a2e57
Daniele Muffato <danixgithub1@gmail.com>
2025-08-01 18:52:17 +0200

    modify init script to automatically do few init ops

--- scripts/fedora-kde/init.sh
+++ scripts/fedora-kde/init.sh
@@ -5,6 +5,8 @@ sudo -v &&
     wget -O "$TMP_DIR/dotfiles.zip" https://github.com/daniele821/dotfiles/archive/refs/heads/fedora-kde.zip &&
     unzip -d "$TMP_DIR" "$TMP_DIR/dotfiles.zip" &&
     NO_CHECKS="" "${TMP_DIR}/dotfiles-fedora-kde/autosaver" run &&
+    echo fedora-kde >~/.personal/repos/daniele821/dotfiles/.branch &&
+    rm -rf ~/.config/nvim && ln -s ~/.personal/repos/daniele821/nvim-config/ ~/.config/nvim && nvim --headless +qa &&
     for i in {30..1}; do
         echo -en "\r\e[Krebooting in $i seconds... ctrl+c to skip"
         sleep 1
