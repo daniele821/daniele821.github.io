f38b70d86fb0d8a40fb11d19386b5fed502c48fc
Daniele Muffato <danixgithub1@gmail.com>
2025-08-01 23:03:15 +0200

    fedora-kde repo script now sets correct branch for dotfiles

--- scripts/fedora-kde/repo.sh
+++ scripts/fedora-kde/repo.sh
@@ -1,4 +1,5 @@
 TMP_DIR="$(mktemp -d)" &&
     git clone --branch fedora-kde --depth 1 "https://github.com/daniele821/dotfiles" "$TMP_DIR" &&
     NO_CHECKS="" "$TMP_DIR/autosaver" git &&
+    echo fedora-kde >~/.personal/repos/daniele821/dotfiles/.branch &&
     rm -rf "$TMP_DIR" || echo -e "\e[1;31mfailed to download git repos\e[m"
