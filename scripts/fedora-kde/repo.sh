66b31023e42e400837abeab272198e751ee7dda7
- <->
2025-08-14 21:14:36 +0200

    removed repo script

--- scripts/fedora-kde/repo.sh
+++
@@ -1,4 +0,0 @@
-TMP_DIR="$(mktemp -d)" &&
-    git clone --branch fedora-kde --depth 1 "https://github.com/daniele821/dotfiles" "$TMP_DIR" &&
-    NO_CHECKS="" "$TMP_DIR/autosaver" git &&
-    rm -rf "$TMP_DIR" || echo -e "\e[1;31mfailed to download git repos\e[m"
