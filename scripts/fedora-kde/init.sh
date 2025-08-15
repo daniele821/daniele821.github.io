5569157be1a7aa2f36a66366b08a96bb98c5c7a7
- <->
2025-08-15 21:13:21 +0200

    updated init script

--- scripts/fedora-kde/init.sh
+++ scripts/fedora-kde/init.sh
@@ -5,6 +5,7 @@ sudo -v &&
     wget -O "$TMP_DIR/dotfiles.zip" https://github.com/daniele821/dotfiles/archive/refs/heads/fedora-kde.zip &&
     unzip -d "$TMP_DIR" "$TMP_DIR/dotfiles.zip" &&
     "${TMP_DIR}/dotfiles-fedora-kde/autosaver" run &&
+    "${TMP_DIR}/dotfiles-fedora-kde/autosaver" restoreall &&
     for i in {30..1}; do
         echo -en "\r\e[Krebooting in $i seconds... ctrl+c to skip"
         sleep 1
